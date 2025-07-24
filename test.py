66c34b89fd8f40b546a1fa4bf8bbf0f9da245058
3NVZ <141152150+3nvz@users.noreply.github.com>
2025-07-24 11:12:37 -0700

    Delete test.py
    deleted secrets

--- test.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import Client import os
-# Your Account SID from twilio.com/console
-account_sid = "AC0fd8259feac0af2db824199c6a49d7f0"
-# Your Auth Token from twilio.com/console
-auth_token = "67d734b3ed35c92b18bf12024e7fc128"
-client = Client (account_sid, auth_token)
-message client.messages.create(
-to="+12345995159", from_="+12345995159", body="Hello from Python!")
-print (message.sid)%
